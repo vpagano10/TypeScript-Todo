7162bb9f3e0367d6a84153d53f19acb0f35a2fe9
vpagano10 <vpagano10@gmail.com>
2020-08-10 15:54:21 -0400

    Setup of todo app and basic functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { TodoList } from "./TodoList";
+import { AddTodoForm } from "./AddTodoForm";
+
+const initialTodos: Array<Todo> = [
+  { text: "Walk the dog", complete: true },
+  { text: "Write App", complete: false },
+];
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo: ToggleTodo = (selectedTodo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo == selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const addTodo: AddTodo = (newTodo) => {
+    newTodo.trim() !== "" &&
+      setTodos([...todos, { text: newTodo, complete: false }]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <AddTodoForm addTodo={addTodo} />
+    </React.Fragment>
   );
 }
 
 export default App;
+
+// props = like arguments to a function
+// they are what you want to initialize your component to, or what you want your component to render like
+
+// state = state is handled inside of a component
+// ex: the current count is handled by the state, while an initial count (as a prop) is passed in
